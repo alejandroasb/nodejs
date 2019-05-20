b721ca2b67d1b4d51143ff3022d084a0489de691
Alejandro sanchez <alejandroasb@gmail.com>
2019-05-19 23:27:30 -0500

    primera

---
+++ Documents/nodejs/cursos.js
@@ -0,0 +1,13 @@
+let notas=[{
+        nombre:'Juan',
+        matematicas:4,
+        ingles:3,
+        programacion:2
+    },
+    {
+        nombre:'Maria',
+        matematicas:5,
+        ingles:1,
+        programacion:3
+    }
+];
